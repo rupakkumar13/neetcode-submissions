fdc74609430ea3836c18e10e870d3c3fd85acd7a
Rupak Kumar <rupakkumar9813@gmail.com>
2026-09-15 00:54:14 +0530

    Add: longest-palindromic-subsequence - submission-5

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+       int n = s.size();
+       vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+       for(int i = 0; i < n; i++){
+        dp[i][0] = 0;
+        dp[0][i] = 0;
+       } 
+
+       for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(s[i-1] == s[n-j]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+
+            else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+       }
+       return dp[n][n];
+    }
+};
