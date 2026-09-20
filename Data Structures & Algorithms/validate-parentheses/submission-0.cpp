f5c9cf67c8131064edb6e9772adb60e01b3629e2
Rupak Kumar <rupakkumar9813@gmail.com>
2026-09-20 19:20:03 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+
+        for (int i = 0; i < n; i++) {
+
+            // Opening bracket
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+
+          
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+
+                if ((s[i] == ')' && st.top() == '(') ||
+                    (s[i] == '}' && st.top() == '{') ||
+                    (s[i] == ']' && st.top() == '[')) {
+
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
